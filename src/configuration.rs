b9266305b4fb2edde07c9ca971913794a6cc351f
David Oyinbo <dev.davexoyinbo@gmail.com>
2023-06-19 12:23:57 +0100

    Configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,14 +1,33 @@
-#[derive(serde::Deserialize)]
+use config::Config;
+
+#[derive(serde::Deserialize, Debug)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub applicationn_port: u16
+    pub application_port: u16,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
     pub port: u16,
     pub host: String,
-    pub database_name: String
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres:{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("configuration"))
+        .build()?;
+
+    settings.try_deserialize()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 
-use z2p::startup;
+use z2p::{startup, configuration::get_configuration};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    println!("Configuration: {:#?}", configuration);
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Unable to bind address");
     startup::run(listener)?.await
 }//end main method
