018db5ce99a62fb9ff364c452a7b3bfb4ea11b60
David Oyinbo <dev.davexoyinbo@gmail.com>
2023-06-22 23:13:38 +0100

    Added logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::{PgPool};
 use z2p::{configuration::get_configuration, startup};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
     let connection_pool = PgPool::connect(&connection_string)
--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,26 @@
-use std::{net::TcpListener, io};
-use actix_web::{dev::Server, HttpServer, App, web};
-use sqlx::{PgPool};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use sqlx::PgPool;
+use std::{io, net::TcpListener};
 
 use crate::routes;
-
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, io::Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
-        App::new().route("/health_check", web::get().to(routes::health_check::health_check))
-        .route("subscriptions", web::post().to(routes::subscriptions::subscribe))
-        .app_data(db_pool.clone())
+        App::new()
+            .wrap(Logger::default())
+            .route(
+                "/health_check",
+                web::get().to(routes::health_check::health_check),
+            )
+            .route(
+                "subscriptions",
+                web::post().to(routes::subscriptions::subscribe),
+            )
+            .app_data(db_pool.clone())
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
