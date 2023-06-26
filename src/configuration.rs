4a791a38e5d0f04567f22b2d30ee9fe7643dac57
David Oyinbo <dev.davexoyinbo@gmail.com>
2023-06-26 11:30:52 +0100

    docker conf

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,10 +1,12 @@
+use std::env;
+
 use config::Config;
-use secrecy::{Secret, ExposeSecret};
+use secrecy::{ExposeSecret, Secret};
 
 #[derive(serde::Deserialize, Debug)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -16,26 +18,76 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+#[derive(serde::Deserialize, Debug)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Local => "local",
+            Self::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production``",
+                other
+            )),
+        }
+    }
+}
+
 impl DatabaseSettings {
     pub fn connection_string(&self) -> Secret<String> {
         Secret::new(format!(
             "{}/{}",
-            self.connection_string_without_db().expose_secret(), self.database_name
+            self.connection_string_without_db().expose_secret(),
+            self.database_name
         ))
     }
 
     pub fn connection_string_without_db(&self) -> Secret<String> {
         Secret::new(format!(
             "postgres://{}:{}@{}:{}",
-            self.username, self.password.expose_secret(), self.host, self.port
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port
         ))
     }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let settings = Config::builder()
-        .add_source(config::File::with_name("configuration"))
-        .build()?;
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
 
-    settings.try_deserialize()
+    let settings = Config::builder()
+        .add_source(config::File::from(configuration_directory.join("base")).required(true));
+
+    let environment: Environment = env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    let settings = settings.add_source(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    );
+
+    settings.build()?.try_deserialize()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::TcpListener;
 
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
-use z2p::{configuration::get_configuration, startup, get_subscriber, init_subscriber};
+use z2p::{configuration::get_configuration, get_subscriber, init_subscriber, startup};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,10 +11,12 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&connection_string.expose_secret())
-        .await
-        .expect("Unable to connect to postgres");
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))
-        .expect("Unable to bind address");
+    let connection_pool = PgPool::connect_lazy(&connection_string.expose_secret())
+        .expect("Failed to create Postgres connection pool.");
+    let listener = TcpListener::bind(format!(
+        "{}:{}",
+        &configuration.application.host, &configuration.application.port
+    ))
+    .expect("Unable to bind address");
     startup::run(listener, connection_pool)?.await
 } //end main method
